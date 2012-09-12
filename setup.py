3ee89adf5b412484ad1ee0493ef18e08440ad45d
Enrico Franchi <enrico.franchi@gmail.com>
2012-09-12 10:25:09 +0200

    Improved setup file.

--- setup.py
+++ setup.py
@@ -5,18 +5,23 @@ version = '0.1'
 
 setup(name='ParamUnittest',
       version=version,
-      description="Simple unittest extension to have parametrized unit tests.",
+      description="Simple extension to have parametrized unit tests.",
       long_description="""\
-      This package allows to create parametrized unit-tests that work with the
-      standard unittest package. A parametrized test case is automatically converted
-      to multiple test cases. Since they are TestCase subclasses, they work with
-      other test suites that recognize TestCases.
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      This package allows to create parametrized
+      unit-tests that work with the standard unittest package. A
+      parametrized test case is automatically converted to multiple test
+      cases. Since they are TestCase subclasses, they work with other
+      test suites that recognize TestCases.
+      """,
+      classifiers=[
+        'Topic :: Software Development :: Testing',
+      ],
+      # Get strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='unittest',
       author='Enrico Franchi',
       author_email='enrico.franchi@gmail.com',
-      url='',
+      url='https://github.com/rik0/ParamUnittest',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
