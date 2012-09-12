ab9fbed1a0b2367ada19f0f6321fe7c2fa08a2f6
Enrico Franchi <enrico.franchi@gmail.com>
2012-09-12 10:59:29 +0200

    Improved setup file.

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
 import sys, os
+from os import path
+
 
 version = '0.1'
+
+examples_directory = 'examples'
+example_filename = 'example.py'
+
+def example_text(examples_directory=examples_directory,
+                 example_filename=example_filename):
+    rel_path = path.join(examples_directory, example_filename)
+    with open(rel_path) as fh:
+        return fh.read()
+
+long_description  = """\
+This package allows to create parametrized unit-tests that work with the
+standard unittest package. A parametrized test case is automatically
+converted to multiple test cases. Since they are TestCase subclasses,
+they work with other test suites that recognize TestCases.
+
+
+== Examples ==
+
+%s
+      """ % example_text()
+
 
 setup(name='ParamUnittest',
       version=version,
       description="Simple extension to have parametrized unit tests.",
-      long_description="""\
-      This package allows to create parametrized
-      unit-tests that work with the standard unittest package. A
-      parametrized test case is automatically converted to multiple test
-      cases. Since they are TestCase subclasses, they work with other
-      test suites that recognize TestCases.
-      """,
+      long_description=long_description,
       classifiers=[
         'Topic :: Software Development :: Testing',
       ],
